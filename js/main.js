469e77187f0d5da084a24df91ba9f685e4a72acb
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-04-18 22:48:21 -0500

    cancha con 3 puntitos

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var cancha = document.getElementById("cancha");
 var borde = document.getElementById("borde");
 var porteria = document.getElementsByClassName("porteria");
 var circulo = document.getElementById("mediaCancha");
+var puntito = document.getElementsByClassName("puntito");
 
 cancha.style.position = "absolute";
 cancha.style.top = "100px";
@@ -22,3 +23,14 @@ circulo.style.position = "absolute";
 circulo.style.top = "38%";
 circulo.style.left = "45%";
 
+puntito[0].style.position = "absolute";
+puntito[0].style.top = "0";
+puntito[0].style.right = "-10px";
+
+puntito[1].style.position = "absolute";
+puntito[1].style.top = "-90px";
+puntito[1].style.bottom = "50px";
+
+puntito[2].style.position = "absolute";
+puntito[2].style.bottom = "0";
+puntito[2].style.left = "-10px";
